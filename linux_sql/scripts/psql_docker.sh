7bf9d540bc18856595aa5999dfa7cbedfc03bcd1
Barbod Rouzbahani <barbodrouzbahani1378@gmail.com>
2022-07-21 05:22:47 +0000

    psql_docker update

--- linux_sql/scripts/psql_docker.sh
+++ linux_sql/scripts/psql_docker.sh
@@ -1,4 +1,47 @@
 #!/bin/bash
 
-echo "Hello world!"
+cmd=$1
+db_username=$2
+db_password=$3
+
+echo "$(sudo systemctl status docker || sudo systemctl start docker)"
+echo "$(docker container inspect jrvs-psql)"
+
+container_status=$?
+
+case $cmd in 
+  create)
+  
+  if [ $container_status -eq 0 ]; then
+	echo 'Container already exists'
+	exit 1	
+  fi
+
+  if [ $# -ne 3 ]; then
+  	echo 'Create requires username and password'
+    	exit 1
+  fi
+  
+echo "$(docker volume create pgdata)"
+echo "$(docker run --name $db_username -e POSTGRES_PASSWORD=$db_password -d -v pgdata:/var/lib/postgresql/data -p 5432:5432 postgres:9.6-alpine)"
+  exit $?
+  ;;
+
+  start|stop) 
+  if [ $container_status -ne 0 ]; then
+	echo "Container needs to be created first"
+	exit 1
+  fi
+
+	docker container $cmd jrvs-psql
+	exit $?
+	;;	
+  
+  *)
+	echo 'Illegal command'
+	echo 'Commands: start|stop|create'
+	exit 1
+	;;
+esac
+
 exit 0
